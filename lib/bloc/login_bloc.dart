6c48ffbc793667abbbaab9583f367bc9829ecb5b
IAMAntaRiksa <imamakbarmegaantariksa@gmail.com>
2022-01-03 20:36:25 +0700

    update

--- lib/bloc/login_bloc.dart
+++ lib/bloc/login_bloc.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:login_flutter/network/api_client_response.dart';
+import 'package:login_flutter/data/respository/api_client_response.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
@@ -10,34 +11,25 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   final ApiClientResponse _apiClientResponse = ApiClientResponse();
   LoginBloc() : super(LoginInitial()) {
     on<GetLoginEvent>(_getMapLogin);
-    // on<GetRegestrasiUserEvent>(_getMapRegistrasi);
   }
 
   FutureOr<void> _getMapLogin(GetLoginEvent event, emit) async {
     emit(LoginLoding());
-    final check = await _apiClientResponse.sendLogin(
-        username: event.username, password: event.password);
     try {
-      emit(LoginSuccsess(token: check.toString()));
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      final auth = await _apiClientResponse.sendLogin(
+          email: event.email, password: event.password);
+      String token = prefs.setString('token', auth ?? '').toString();
+      prefs.getString('token');
+      if(token.isEmpty){
+        token.isEmpty(){
+          toke(){
+            
+          }
+        }
+      }
     } catch (e) {
       emit(const LoginError(message: 'Error Data Login'));
     }
   }
-
-//   FutureOr<void> _getMapRegistrasi(GetRegestrasiUserEvent event, emit) async {
-//     emit(LoginLoding());
-//       final check = await _apiClientResponse.registrasiUser(
-//         username: event.username,
-//         password: event.password,
-//         confirmPass: event.confrimPassword,
-//         email: event.email);
-//     try {
-//       if(check != null){
-//         emit
-//       }
-//     } catch (e) {
-//       emit(RegisterError(message: e.toString()));
-//     }
-//   }
-// }
 }
